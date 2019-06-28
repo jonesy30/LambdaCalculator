d9f784984b0ce8e6e763b7e3427432493bebb384
Yola <yola.jones48@gmail.com>
2019-06-28 18:09:22 +0100

    Alpha reduction working, the code is really ugly and needs a LOT of cleaning up, but for now, it works

--- tree_test.py
+++ tree_test.py
@@ -2,29 +2,39 @@ from ScopeObject import ScopeObject
 from CurrentLetter import CurrentLetter
 
 class TreeNode(object):
-    def __init__(self, data, parent):
+    def __init__(self, data, parent, sibling_scope_object, current_letter):
         self.data = data
         self.parent = parent
         self.children = []
+        self.sibling_scope_object = sibling_scope_object
+        self.current_letter = current_letter
+        self.processed_data = None
 
     def add_child(self, data, parent):
-        child = TreeNode(data, parent)
+        s = ScopeObject(self.current_letter)
+        child = TreeNode(data, parent, s, self.current_letter)
         self.children.append(child)
         return self.children[-1]
     
     def get_parent(self):
         return self.parent
     
-    # def append_data(self, new_data):
-    #     self.data = self.data + new_data
-    
-    def print_level(self):
+    def print_siblings(self):
         if self.parent is not None:
             output = ""
             for child in self.parent.children:
                 output = output + str(child)
             
             return output
+    
+    def get_siblings(self):
+        siblings = None
+        if self.parent is not None:
+            siblings = []
+            for child in self.parent.children:
+                siblings.append(child)
+            
+        return siblings
 
     def print_node(self):
         print(self.data, end = '')
@@ -59,65 +69,94 @@ class TreeNode(object):
 
         for i,letter in enumerate(abstraction_list):
             if letter.isalpha():
-                if letter not in scope_object.bound_values:
-                    #REPEATED CODE!!!!
-                    if letter not in scope_object.original_letters:
-
-                        print(letter+" not found")
-                        print("Original_letters "+str(scope_object.original_letters))
-                        print("Associated_letters "+str(scope_object.associated_letters))
-
-                        scope_object.original_letters.append(letter)
-                        new_letter = scope_object.current_letter.getUpperCase()
-                        scope_object.associated_letters.append(new_letter)
-                        abstraction_list[i] = new_letter
-                    else:
-                        print(letter+" found")
-                        print("Original_letters "+str(scope_object.original_letters))
-                        print("Associated_letters "+str(scope_object.associated_letters))
-                        abstraction_list[i] = scope_object.associated_letters[scope_object.original_letters.index(letter)]
+                if letter not in scope_object.original_letters:
+                    print("LETTER "+letter+" NOT IN FOUND VALUES")
                 else:
-                    #REPEATED CODE!!!
-                    if letter not in scope_object.original_letters:
-
-                        print(letter+" not found")
-                        print("Original_letters "+str(scope_object.original_letters))
-                        print("Associated_letters "+str(scope_object.associated_letters))
-
-                        scope_object.original_letters.append(letter)
-                        new_letter = scope_object.current_letter.getLowerCase()
-                        scope_object.associated_letters.append(new_letter)
-                        abstraction_list[i] = new_letter
-                    else:
-                        print(letter+" found")
-                        print("Original_letters "+str(scope_object.original_letters))
-                        print("Associated_letters "+str(scope_object.associated_letters))
-                        abstraction_list[i] = scope_object.associated_letters[scope_object.original_letters.index(letter)]
+                    abstraction_list[i] = scope_object.associated_letters[scope_object.original_letters.index(letter)]
 
         abstraction = "".join(abstraction_list)
         return abstraction
+    
+    def process_level(self, level):
 
-    def process_node(self, current_letter):
+        level_list = []
+        if level is not None:
+            for sibling in level:
+                for letter in sibling.data:
+                    level_list.append(letter)
+            #level_list = list(level)
+        
+        for i,letter in enumerate(level_list):
+            if letter.isalpha():
+                if letter not in self.sibling_scope_object.bound_values:
+                    if letter not in self.sibling_scope_object.original_letters:
+
+                        #print(letter+" not found")
+                        #print("Original_letters "+str(self.sibling_scope_object.original_letters))
+                        #print("Associated_letters "+str(self.sibling_scope_object.associated_letters))
+
+                        self.sibling_scope_object.original_letters.append(letter)
+                        new_letter = self.sibling_scope_object.current_letter.getUpperCase()
+                        self.sibling_scope_object.associated_letters.append(new_letter)
+                else:
+                    if letter not in self.sibling_scope_object.original_letters:
+
+                        #print(letter+" not found")
+                        #print("Original_letters "+str(self.sibling_scope_object.original_letters))
+                        #print("Associated_letters "+str(self.sibling_scope_object.associated_letters))
+
+                        self.sibling_scope_object.original_letters.append(letter)
+                        new_letter = self.sibling_scope_object.current_letter.getLowerCase()
+                        self.sibling_scope_object.associated_letters.append(new_letter)
+        
+
+    def process_node(self):
+        print()
+        print()
         print("Processing node")
 
-        returned_bound_values = self.get_bound_values(self.print_level())
-        scope_object = ScopeObject(current_letter)
-        scope_object.bound_values = returned_bound_values
+        returned_bound_values = self.get_bound_values(self.print_siblings())
+        #scope_object = ScopeObject(current_letter)
+        self.sibling_scope_object.bound_values = returned_bound_values
+        self.process_level(self.get_siblings())
+        print("Me = "+str(self.data))
+        print("Level = "+str(self.print_siblings()))
+        print("Original letters = "+str(self.sibling_scope_object.original_letters))
+        print("Associated letters = "+str(self.sibling_scope_object.associated_letters))
+        print("Processed_data = "+str(self.processed_data))
+        #children_print = self.process_abstraction(self.data, self.sibling_scope_object)
+       
 
-        children_print = self.process_abstraction(self.data, scope_object)
+        if self.processed_data == None:
+            siblings = self.get_siblings()
+            if siblings is not None:
+                for sibling in siblings:
+                    sibling.processed_data = self.process_abstraction(sibling.data,self.sibling_scope_object)
+                    #abstraction = sibling.process_node()
+                    #if abstraction is not None:
+                    #    abstraction_full = abstraction_full + abstraction
+
+        if self.processed_data == None:
+            abstraction_full = self.process_abstraction(self.data, self.sibling_scope_object)
+        else:
+            abstraction_full = self.processed_data
+            
         for child in self.children:
-            abstraction = child.process_node(current_letter)
-            children_print = children_print + abstraction
-        return children_print
-        
-        
+           abstraction = child.process_node()
+           if abstraction is not None:
+               abstraction_full = abstraction_full + abstraction
+        #return children_print
+
+        return abstraction_full
+
         
     def __repr__(self):
         return repr(self.data)
     
 class Tree(object):
-    def __init__(self):
-        self.root = TreeNode("",None)
+    def __init__(self, current_letter):
+        s = ScopeObject(current_letter)
+        self.root = TreeNode("",None,s,current_letter)
         self.height = 0
         self.levels = []
         self.current_node = self.root
@@ -142,7 +181,8 @@ class Tree(object):
 
 expression = input("Enter test expression: ")
 
-t = Tree()
+current_letter = CurrentLetter()
+t = Tree(current_letter)
 t.add_child("")
 for i,expression_letter in enumerate(expression):
     if expression_letter == '(':
@@ -160,8 +200,8 @@ t.get_root().print_node()
 print()
 print("Processed node")
 
-current_letter = CurrentLetter()
-print(t.get_root().process_node(current_letter))
+
+print(t.get_root().process_node())
 
 # print()
 # print("Level = ")
