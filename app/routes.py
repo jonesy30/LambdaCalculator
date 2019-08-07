ebc77f212fc314401dda80d340726480396112a9
Yola <yola.jones48@gmail.com>
2019-08-07 12:12:20 +0100

    Fixing bug when clicking submit with empty input

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,14 @@ from LambdaCalculus import web_interface
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        #output = hello_world_test.hello_world(request.form["input-box"])
         lambda_term = request.form["input-box"]
-        result,type_value,type_check = web_interface(lambda_term,"v")
-        return render_template('index.html',title='Home',input=lambda_term,result="Result = "+result+"<br>"+"Valid typing = "+type_check+"<br>"+"Type returned = "+type_value+"<br>")
-    
-    return render_template('index.html',title='Home',input="enter_expression_here",user="Jonesy",result="")
+        if lambda_term != "":
+            result,type_value,type_check = web_interface(lambda_term,"v")
+            return render_template('index.html',title='Home',input=lambda_term,result="Result = "+result+"<br>"+"Valid typing = "+type_check+"<br>"+"Type returned = "+type_value+"<br>")
+        else:
+            return render_template('index.html',title='Home',input="enter_expression_here",result="")
+
+    return render_template('index.html',title='Home',input="enter_expression_here",result="")
 
 @app.route("/lambda_output/", methods=['POST'])
 def process_lambda():
